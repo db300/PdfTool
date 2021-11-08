af0a907ccaa99eb5ab0ea028f144e648eab582eb
DavidHawkes <db300@163.com>
2021-11-08 17:35:08 +0800

    增加PDF文件生成

---
+++ PdfConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdfConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path0 = @"C:\Users\冷怀晶\Downloads\B端字体著作权登记证书及更名说明（285份）\证书\";
+            var path1 = @"C:\Users\冷怀晶\Downloads\B端字体著作权登记证书及更名说明（285份）\更名说明\";
+            var path2 = @"C:\Users\冷怀晶\Downloads\B端字体著作权登记证书及更名说明（285份）\合并\";
+            var fileList0 = Directory.GetFiles(path0).ToList();
+            var fileList1 = Directory.GetFiles(path1).ToList();
+            foreach(var file in fileList0)
+            {
+                var ext = Path.GetExtension(file).ToLower();
+                var fileName = Path.GetFileNameWithoutExtension(file);
+                var fileName2 = fileList1.FirstOrDefault(a => a.Contains(fileName));
+                var outFileName = $"{path2}{fileName}.pdf";
+                switch (ext)
+                {
+                    case ".pdf":
+                        PdfHelperLibrary.Builder.InsertImage2Pdf(file, new List<string> { fileName2 }, outFileName);
+                        break;
+                    default:
+                        PdfHelperLibrary.Builder.Image2Pdf(new List<string> { file, fileName2 }, outFileName);
+                        break;
+                }
+            }
+            /*
+            PdfHelperLibrary.Builder.Image2Pdf(new List<string> {
+                @"C:\Users\冷怀晶\Downloads\B端字体著作权登记证书及更名说明（285份）\更名说明\1-2009-F-020560-汉仪菱心体（简）.jpeg",
+                @"C:\Users\冷怀晶\Downloads\B端字体著作权登记证书及更名说明（285份）\证书\1-2009-F-020560-汉仪菱心体（简）.jpg"}, "a.pdf");
+            */
+        }
+    }
+}
---
+++ PdfHelperLibrary/Builder.cs
@@ -0,0 +1,55 @@
+﻿using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdfHelperLibrary
+{
+    /// <summary>
+    /// 生成器
+    /// </summary>
+    public class Builder
+    {
+        public static void Image2Pdf(List<string> imgFileList, string outFileName)
+        {
+            using (var pdf = new PdfDocument())
+            {
+                for (var i = 0; i < imgFileList.Count; i++)
+                {
+                    var page = pdf.AddPage();
+                    var graphics = XGraphics.FromPdfPage(page);
+
+                    var image = XImage.FromFile(imgFileList[i]);
+                    if (image.PixelWidth > image.PixelHeight) page.Orientation = PdfSharp.PageOrientation.Landscape;
+
+                    graphics.DrawImage(image, 0, 0, page.Width, page.Height);
+                    //graphics.DrawImage(image, 0, 0);
+                }
+                pdf.Save(outFileName);
+            }
+        }
+
+        public static void InsertImage2Pdf(string inFileName, List<string> imgFileList, string outFileName)
+        {
+            using (var pdf = PdfReader.Open(inFileName))
+            {
+                for (var i = 0; i < imgFileList.Count; i++)
+                {
+                    var page = pdf.AddPage();
+                    var graphics = XGraphics.FromPdfPage(page);
+
+                    var image = XImage.FromFile(imgFileList[i]);
+                    if (image.PixelWidth > image.PixelHeight) page.Orientation = PdfSharp.PageOrientation.Landscape;
+
+                    graphics.DrawImage(image, 0, 0, page.Width, page.Height);
+                    //graphics.DrawImage(image, 0, 0);
+                }
+                pdf.Save(outFileName);
+            }
+        }
+    }
+}

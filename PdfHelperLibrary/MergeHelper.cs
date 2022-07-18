8bd73307e0b22f3b1fa1845e3113c28f6f716e9b
DavidHawkes <db300@163.com>
2022-07-18 14:58:47 +0800

    抽离拆分帮助类和合并帮助类

---
+++ PdfHelperLibrary/MergeHelper.cs
@@ -0,0 +1,35 @@
+﻿using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace PdfHelperLibrary
+{
+    /// <summary>
+    /// 合并帮助类
+    /// </summary>
+    public static class MergeHelper
+    {
+        public static void MergePdf(List<string> inputPdfFilenameList)
+        {
+            var outputDocument = new PdfDocument();
+            foreach (var file in inputPdfFilenameList)
+            {
+                var inputDocument = PdfReader.Open(file, PdfDocumentOpenMode.Import);
+                var pageCount = inputDocument.PageCount;
+                for (var i = 0; i < pageCount; i++)
+                {
+                    outputDocument.AddPage(inputDocument.Pages[i]);
+                }
+            }
+            var path = Path.GetDirectoryName(inputPdfFilenameList.First());
+            if (!path.EndsWith(@"\")) path = $@"{path}\";
+            var filename = $"{path}MergedFile - {DateTime.Now:yyyyMMddHHmmssfff}.pdf";
+            outputDocument.Save(filename);
+            Process.Start(filename);
+        }
+    }
+}
---
+++ PdfHelperLibrary/SplitHelper.cs
@@ -0,0 +1,61 @@
+﻿using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System;
+using System.IO;
+
+namespace PdfHelperLibrary
+{
+    /// <summary>
+    /// 拆分帮助类
+    /// </summary>
+    public static class SplitHelper
+    {
+        public static void SplitPdf(string inputPdfFileName)
+        {
+            var inputDocument = PdfReader.Open(inputPdfFileName, PdfDocumentOpenMode.Import);
+            var pageCount = inputDocument.PageCount;
+            var path = Path.GetDirectoryName(inputPdfFileName);
+            var fileName = Path.GetFileNameWithoutExtension(inputPdfFileName);
+            var prefixFileName = Path.Combine(path, fileName);
+            for (var i = 0; i < pageCount; i++)
+            {
+                // Create new document
+                var outputDocument = new PdfDocument { Version = inputDocument.Version };
+                outputDocument.Info.Title = $"Page {i + 1} of {inputDocument.Info.Title}";
+                outputDocument.Info.Creator = inputDocument.Info.Creator;
+                // Add the page and save it
+                outputDocument.AddPage(inputDocument.Pages[i]);
+                outputDocument.Save($"{prefixFileName} - Page {i + 1}.pdf");
+            }
+        }
+
+        /// <summary>
+        /// 拆分PDF(按页数)
+        /// </summary>
+        /// <param name="inputPdfFileName"></param>
+        /// <param name="pageCountPerDoc">拆分后每个文档页数</param>
+        public static void SplitPdf(string inputPdfFileName, int pageCountPerDoc)
+        {
+            var inputDocument = PdfReader.Open(inputPdfFileName, PdfDocumentOpenMode.Import);
+            var pageCount = inputDocument.PageCount;
+            var path = Path.GetDirectoryName(inputPdfFileName);
+            var fileName = Path.GetFileNameWithoutExtension(inputPdfFileName);
+            var prefixFileName = Path.Combine(path, fileName);
+            for (var i = 0; i < pageCount;)
+            {
+                var subPageCount = Math.Min(pageCountPerDoc, pageCount - i);
+                var pageLabel = subPageCount > 1
+                    ? $"{i + 1} - {i + subPageCount}"
+                    : $"{i + 1}";
+                // Create new document
+                var outputDocument = new PdfDocument { Version = inputDocument.Version };
+                outputDocument.Info.Title = $"Page {pageLabel} of {inputDocument.Info.Title}";
+                outputDocument.Info.Creator = inputDocument.Info.Creator;
+                // Add the page and save it
+                for (var j = 0; j < subPageCount; j++) outputDocument.AddPage(inputDocument.Pages[i + j]);
+                outputDocument.Save($"{prefixFileName} - Page {pageLabel}.pdf");
+                i += subPageCount;
+            }
+        }
+    }
+}

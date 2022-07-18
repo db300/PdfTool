622f41e5e0621191e98caa288f51301c72bfa37c
DavidHawkes <db300@163.com>
2022-07-18 14:59:32 +0800

    调整PDF拆分器代码

--- PdfSplitter/MainForm.cs
+++ PdfSplitter/MainForm.cs
@@ -1,14 +1,8 @@
-﻿using PdfSharp.Pdf;
-using PdfSharp.Pdf.IO;
+﻿using PdfSharp.Pdf.IO;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PdfSplitter
@@ -28,6 +22,9 @@ namespace PdfSplitter
         private readonly List<string> _inputPdfFileList = new List<string>();
         private TextBox _txtLog;
         private NumericUpDown _numPagePerDoc;
+
+        private const int ControlMargin = 20;
+        private const int ControlPadding = 12;
         #endregion
 
         #region event handler
@@ -54,60 +51,10 @@ namespace PdfSplitter
             }
             foreach (var fileName in _inputPdfFileList)
             {
-                SplitPdf(fileName, (int)_numPagePerDoc.Value);
+                PdfHelperLibrary.SplitHelper.SplitPdf(fileName, (int)_numPagePerDoc.Value);
                 _txtLog.AppendText($"{fileName} 拆分完成\r\n");
             }
             _txtLog.AppendText("拆分完成\r\n");
-        }
-        #endregion
-
-        #region method
-        private void SplitPdf(string inputPdfFileName)
-        {
-            var inputDocument = PdfReader.Open(inputPdfFileName, PdfDocumentOpenMode.Import);
-            var pageCount = inputDocument.PageCount;
-            var path = Path.GetDirectoryName(inputPdfFileName);
-            var fileName = Path.GetFileNameWithoutExtension(inputPdfFileName);
-            var prefixFileName = Path.Combine(path, fileName);
-            for (var i = 0; i < pageCount; i++)
-            {
-                // Create new document
-                var outputDocument = new PdfDocument { Version = inputDocument.Version };
-                outputDocument.Info.Title = $"Page {i + 1} of {inputDocument.Info.Title}";
-                outputDocument.Info.Creator = inputDocument.Info.Creator;
-                // Add the page and save it
-                outputDocument.AddPage(inputDocument.Pages[i]);
-                outputDocument.Save($"{prefixFileName} - Page {i + 1}.pdf");
-            }
-        }
-
-        /// <summary>
-        /// 拆分PDF(按页数)
-        /// </summary>
-        /// <param name="inputPdfFileName"></param>
-        /// <param name="pageCountPerDoc">拆分后每个文档页数</param>
-        private void SplitPdf(string inputPdfFileName, int pageCountPerDoc)
-        {
-            var inputDocument = PdfReader.Open(inputPdfFileName, PdfDocumentOpenMode.Import);
-            var pageCount = inputDocument.PageCount;
-            var path = Path.GetDirectoryName(inputPdfFileName);
-            var fileName = Path.GetFileNameWithoutExtension(inputPdfFileName);
-            var prefixFileName = Path.Combine(path, fileName);
-            for (var i = 0; i < pageCount;)
-            {
-                var subPageCount = Math.Min(pageCountPerDoc, pageCount - i);
-                var pageLabel = subPageCount > 1
-                    ? $"{i + 1} - {i + subPageCount}"
-                    : $"{i + 1}";
-                // Create new document
-                var outputDocument = new PdfDocument { Version = inputDocument.Version };
-                outputDocument.Info.Title = $"Page {pageLabel} of {inputDocument.Info.Title}";
-                outputDocument.Info.Creator = inputDocument.Info.Creator;
-                // Add the page and save it
-                for (var j = 0; j < subPageCount; j++) outputDocument.AddPage(inputDocument.Pages[i + j]);
-                outputDocument.Save($"{prefixFileName} - Page {pageLabel}.pdf");
-                i += subPageCount;
-            }
         }
         #endregion
 
@@ -117,12 +64,12 @@ namespace PdfSplitter
             MaximizeBox = false;
             ShowIcon = false;
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "PDF拆分器";
+            Text = $"PDF拆分器 {System.Reflection.Assembly.GetExecutingAssembly().GetName().Version}";
 
             var btnAddFile = new Button
             {
                 AutoSize = true,
-                Location = new Point(20, 20),
+                Location = new Point(ControlMargin, ControlMargin),
                 Parent = this,
                 Text = "添加文件"
             };
@@ -131,7 +78,7 @@ namespace PdfSplitter
             var btnSplit = new Button
             {
                 AutoSize = true,
-                Location = new Point(btnAddFile.Right + 12, btnAddFile.Top),
+                Location = new Point(btnAddFile.Right + ControlPadding, btnAddFile.Top),
                 Parent = this,
                 Text = "开始拆分"
             };
@@ -140,7 +87,7 @@ namespace PdfSplitter
             var gpOptions = new GroupBox
             {
                 Anchor = AnchorStyles.Left | AnchorStyles.Top | AnchorStyles.Right,
-                Location = new Point(btnAddFile.Left, btnAddFile.Bottom + 12),
+                Location = new Point(btnAddFile.Left, btnAddFile.Bottom + ControlPadding),
                 Parent = this,
                 Size = new Size(ClientSize.Width - 2 * btnAddFile.Left, 50),
                 Text = "拆分选项"
@@ -149,7 +96,7 @@ namespace PdfSplitter
             _numPagePerDoc = new NumericUpDown
             {
                 AutoSize = true,
-                Location = new Point(20, 20),
+                Location = new Point(ControlMargin, ControlMargin),
                 Maximum = 1000,
                 Minimum = 1,
                 Parent = gpOptions,
@@ -168,7 +115,7 @@ namespace PdfSplitter
             _txtLog = new TextBox
             {
                 Anchor = AnchorStyles.Left | AnchorStyles.Top | AnchorStyles.Right | AnchorStyles.Bottom,
-                Location = new Point(btnAddFile.Left, gpOptions.Bottom + 12),
+                Location = new Point(btnAddFile.Left, gpOptions.Bottom + ControlPadding),
                 Multiline = true,
                 Parent = this,
                 ReadOnly = true,

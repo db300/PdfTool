e018d6c09d9d47eb9748ff36f97d878c612675a3
DavidHawkes <db300@163.com>
2022-10-24 15:34:14 +0800

    增加dotnet framework4.5版本类库项目

---
+++ PdfHelperLibrary.DFX45/MergeHelper.cs
@@ -0,0 +1,43 @@
+﻿using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace PdfHelperLibrary.DFX45
+{
+    /// <summary>
+    /// 合并帮助类
+    /// </summary>
+    public static class MergeHelper
+    {
+        public static string MergePdf(List<string> inputPdfFilenameList)
+        {
+            try
+            {
+                var outputDocument = new PdfDocument();
+                foreach (var file in inputPdfFilenameList)
+                {
+                    var inputDocument = PdfReader.Open(file, PdfDocumentOpenMode.Import);
+                    var pageCount = inputDocument.PageCount;
+                    for (var i = 0; i < pageCount; i++)
+                    {
+                        outputDocument.AddPage(inputDocument.Pages[i]);
+                    }
+                }
+                var path = Path.GetDirectoryName(inputPdfFilenameList.First());
+                if (!path.EndsWith(@"\")) path = $@"{path}\";
+                var filename = $"{path}MergedFile - {DateTime.Now:yyyyMMddHHmmssfff}.pdf";
+                outputDocument.Save(filename);
+                Process.Start(filename);
+                return "";
+            }
+            catch (Exception ex)
+            {
+                return $"合并失败，原因：{ex.Message}";
+            }
+        }
+    }
+}
---
+++ PdfHelperLibrary.DFX45/SplitHelper.cs
@@ -0,0 +1,69 @@
+﻿using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System;
+using System.IO;
+
+namespace PdfHelperLibrary.DFX45
+{
+    /// <summary>
+    /// 拆分帮助类
+    /// </summary>
+    public static class SplitHelper
+    {
+        public static void SplitPdf(string inputPdfFileName)
+        {
+            var inputDocument = PdfReader.Open(inputPdfFileName, PdfDocumentOpenMode.Import);
+            var pageCount = inputDocument.PageCount;
+            var path = Path.GetDirectoryName(inputPdfFileName);
+            var fileName = Path.GetFileNameWithoutExtension(inputPdfFileName);
+            var prefixFileName = Path.Combine(path, fileName);
+            for (var i = 0; i < pageCount; i++)
+            {
+                // Create new document
+                var outputDocument = new PdfDocument { Version = inputDocument.Version };
+                outputDocument.Info.Title = $"Page {i + 1} of {inputDocument.Info.Title}";
+                outputDocument.Info.Creator = inputDocument.Info.Creator;
+                // Add the page and save it
+                outputDocument.AddPage(inputDocument.Pages[i]);
+                outputDocument.Save($"{prefixFileName} - Page {i + 1}.pdf");
+            }
+        }
+
+        /// <summary>
+        /// 拆分PDF(按页数)
+        /// </summary>
+        /// <param name="inputPdfFileName"></param>
+        /// <param name="pageCountPerDoc">拆分后每个文档页数</param>
+        public static string SplitPdf(string inputPdfFileName, int pageCountPerDoc)
+        {
+            try
+            {
+                var inputDocument = PdfReader.Open(inputPdfFileName, PdfDocumentOpenMode.Import);
+                var pageCount = inputDocument.PageCount;
+                var path = Path.GetDirectoryName(inputPdfFileName);
+                var fileName = Path.GetFileNameWithoutExtension(inputPdfFileName);
+                var prefixFileName = Path.Combine(path, fileName);
+                for (var i = 0; i < pageCount;)
+                {
+                    var subPageCount = Math.Min(pageCountPerDoc, pageCount - i);
+                    var pageLabel = subPageCount > 1
+                        ? $"{i + 1} - {i + subPageCount}"
+                        : $"{i + 1}";
+                    // Create new document
+                    var outputDocument = new PdfDocument { Version = inputDocument.Version };
+                    outputDocument.Info.Title = $"Page {pageLabel} of {inputDocument.Info.Title}";
+                    outputDocument.Info.Creator = inputDocument.Info.Creator;
+                    // Add the page and save it
+                    for (var j = 0; j < subPageCount; j++) outputDocument.AddPage(inputDocument.Pages[i + j]);
+                    outputDocument.Save($"{prefixFileName} - Page {pageLabel}.pdf");
+                    i += subPageCount;
+                }
+                return "";
+            }
+            catch (Exception ex)
+            {
+                return $"拆分失败，原因：{ex.Message}";
+            }
+        }
+    }
+}

194bc02374c76a7647be222b35eafb81e194249b
DavidHawkes <db300@163.com>
2022-10-25 11:09:53 +0800

    添加水印功能（待完善）

---
+++ PdfHelperLibrary/WatermarkHelper.cs
@@ -0,0 +1,159 @@
+﻿using PdfSharp.Charting;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.Content.Objects;
+using PdfSharp.Pdf.IO;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdfHelperLibrary
+{
+    /// <summary>
+    /// 水印帮助类
+    /// </summary>
+    public static class WatermarkHelper
+    {
+        private const int _emSize = 30;
+        private static readonly XFont _font = new XFont("黑体", _emSize, XFontStyle.BoldItalic);
+
+        public static string WatermarkPdf(string inputPdfFileName, string watermark)
+        {
+            try
+            {
+                // Get a fresh copy of the input PDF file.
+                var path = Path.GetDirectoryName(inputPdfFileName);
+                var fileName = Path.GetFileNameWithoutExtension(inputPdfFileName);
+                var prefixFileName = Path.Combine(path, fileName);
+                var outputPdfFileName = $"{prefixFileName}-WatermarkFile - {DateTime.Now:yyyyMMddHHmmssfff}.pdf";
+                File.Copy(inputPdfFileName, outputPdfFileName, true);
+                // Remove ReadOnly attribute from the copy.
+                File.SetAttributes(outputPdfFileName, File.GetAttributes(outputPdfFileName) & ~FileAttributes.ReadOnly);
+
+                var inputDocument = PdfReader.Open(outputPdfFileName);
+                foreach (var page in inputDocument.Pages)
+                {
+                    Watermark1(page, watermark);
+                }
+                inputDocument.Save(outputPdfFileName);
+                Process.Start(outputPdfFileName);
+                return "";
+            }
+            catch (Exception ex)
+            {
+                return $"水印添加失败，原因：{ex.Message}";
+            }
+        }
+
+        private static void Watermark1(PdfPage page, string watermark)
+        {
+            // Variation 1: Draw a watermark as a text string.
+
+            // Get an XGraphics object for drawing beneath the existing content.
+            var gfx = XGraphics.FromPdfPage(page, XGraphicsPdfPageOptions.Prepend);
+
+            // Get the size (in points) of the text.
+            var size = gfx.MeasureString(watermark, _font);
+
+            // Define a rotation transformation at the center of the page.
+            gfx.TranslateTransform(page.Width / 2, page.Height / 2);
+            gfx.RotateTransform(-Math.Atan(page.Height / page.Width) * 180 / Math.PI);
+            gfx.TranslateTransform(-page.Width / 2, -page.Height / 2);
+
+            // Create a string format.
+            var format = new XStringFormat
+            {
+                Alignment = XStringAlignment.Near,
+                LineAlignment = XLineAlignment.Near
+            };
+
+            // Create a dimmed red brush.
+            XBrush brush = new XSolidBrush(XColor.FromArgb(128, 255, 0, 0));
+
+            // Draw the string.
+            gfx.DrawString(watermark, _font, brush, new XPoint((page.Width - size.Width) / 2, (page.Height - size.Height) / 2), format);
+
+            gfx.Save();
+        }
+
+        private static void Watermark2(PdfPage page, string watermark)
+        {
+            // Variation 2: Draw a watermark as an outlined graphical path.
+            // NYI: Does not work in Core build.
+
+            // Get an XGraphics object for drawing beneath the existing content.
+            var gfx = XGraphics.FromPdfPage(page, XGraphicsPdfPageOptions.Prepend);
+
+            // Get the size (in points) of the text.
+            var size = gfx.MeasureString(watermark, _font);
+
+            // Define a rotation transformation at the center of the page.
+            gfx.TranslateTransform(page.Width / 2, page.Height / 2);
+            gfx.RotateTransform(-Math.Atan(page.Height / page.Width) * 180 / Math.PI);
+            gfx.TranslateTransform(-page.Width / 2, -page.Height / 2);
+
+            // Create a graphical path.
+            var path = new XGraphicsPath();
+
+            // Create a string format.
+            var format = new XStringFormat();
+            format.Alignment = XStringAlignment.Near;
+            format.LineAlignment = XLineAlignment.Near;
+
+            // Add the text to the path.
+            // AddString is not implemented in PDFsharp Core.
+            path.AddString(watermark, _font.FontFamily, XFontStyle.BoldItalic, 150, new XPoint((page.Width - size.Width) / 2, (page.Height - size.Height) / 2), format);
+
+            // Create a dimmed red pen.
+            var pen = new XPen(XColor.FromArgb(128, 255, 0, 0), 2);
+
+            // Stroke the outline of the path.
+            gfx.DrawPath(pen, path);
+
+            gfx.Save();
+        }
+
+        private static void Watermark3(PdfPage page, string watermark)
+        {
+            // Variation 3: Draw a watermark as a transparent graphical path above text.
+            // NYI: Does not work in Core build.
+
+            // Get an XGraphics object for drawing above the existing content.
+            var gfx = XGraphics.FromPdfPage(page, XGraphicsPdfPageOptions.Append);
+
+            // Get the size (in points) of the text.
+            var size = gfx.MeasureString(watermark, _font);
+
+            // Define a rotation transformation at the center of the page.
+            gfx.TranslateTransform(page.Width / 2, page.Height / 2);
+            gfx.RotateTransform(-Math.Atan(page.Height / page.Width) * 180 / Math.PI);
+            gfx.TranslateTransform(-page.Width / 2, -page.Height / 2);
+
+            // Create a graphical path.
+            var path = new XGraphicsPath();
+
+            // Create a string format.
+            var format = new XStringFormat();
+            format.Alignment = XStringAlignment.Near;
+            format.LineAlignment = XLineAlignment.Near;
+
+            // Add the text to the path.
+            // AddString is not implemented in PDFsharp Core.
+            path.AddString(watermark, _font.FontFamily, XFontStyle.BoldItalic, 150, new XPoint((page.Width - size.Width) / 2, (page.Height - size.Height) / 2), format);
+
+            // Create a dimmed red pen and brush.
+            var pen = new XPen(XColor.FromArgb(50, 75, 0, 130), 3);
+            XBrush brush = new XSolidBrush(XColor.FromArgb(50, 106, 90, 205));
+
+            // Stroke the outline of the path.
+            gfx.DrawPath(pen, brush, path);
+
+            gfx.Save();
+        }
+    }
+}
---
+++ PdfWatermark/MainForm.cs
@@ -0,0 +1,138 @@
+﻿using PdfSharp.Pdf.IO;
+using PdfWatermark.Properties;
+using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace PdfWatermark
+{
+    public partial class MainForm : Form
+    {
+        #region constructor
+        public MainForm()
+        {
+            InitializeComponent();
+
+            InitUi();
+        }
+        #endregion
+
+        #region property
+        private TextBox _txtWatermark;
+        private TextBox _txtFileList;
+        private TextBox _txtLog;
+
+        private const int ControlMargin = 20;
+        private const int ControlPadding = 12;
+        private const string Url4Appreciate = "https://www.yuque.com/docs/share/4d2ad434-a4fe-40a1-b530-c61811d5226e?# 《打赏说明》";
+        #endregion
+
+        #region event handler
+        private void BtnAddFile_Click(object sender, EventArgs e)
+        {
+            var openDlg = new OpenFileDialog { Filter = "PDF文件(*.pdf)|*.pdf|所有文件(*.*)|*.*", Multiselect = true };
+            if (openDlg.ShowDialog() != DialogResult.OK) return;
+            var inputFileList = openDlg.FileNames.ToList();
+            foreach (var fileName in inputFileList)
+            {
+                var document = PdfReader.Open(fileName, PdfDocumentOpenMode.Import);
+                _txtLog.AppendText($"【页数：{document.PageCount}】{fileName}\r\n");
+                _txtFileList.AppendText($"{fileName}\r\n");
+            }
+        }
+
+        private void BtnAddWatermark_Click(object sender, EventArgs e)
+        {
+            var inputPdfFilenameList = _txtFileList.Lines.ToList();
+            inputPdfFilenameList.RemoveAll(string.IsNullOrWhiteSpace);
+            if (inputPdfFilenameList.Count == 0)
+            {
+                _txtLog.AppendText("未添加需要添加水印的PDF文件\r\n");
+                return;
+            }
+            foreach (var file in inputPdfFilenameList)
+            {
+                var s = PdfHelperLibrary.WatermarkHelper.WatermarkPdf(file, _txtWatermark.Text);
+                if (string.IsNullOrWhiteSpace(s)) _txtLog.AppendText($"{file} 水印添加完成\r\n");
+                else _txtLog.AppendText($"{s}\r\n");
+            }
+        }
+
+        private void PicAppreciate_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(Url4Appreciate);
+        }
+        #endregion
+
+        #region ui
+        private void InitUi()
+        {
+            MaximizeBox = false;
+            ShowIcon = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = $"PDF水印工具 {System.Reflection.Assembly.GetExecutingAssembly().GetName().Version}";
+
+            var btnAddFile = new Button
+            {
+                AutoSize = true,
+                Location = new Point(ControlMargin, ControlMargin),
+                Parent = this,
+                Text = "添加文件"
+            };
+            btnAddFile.Click += BtnAddFile_Click;
+
+            _txtWatermark = new TextBox
+            {
+                Anchor = AnchorStyles.Left | AnchorStyles.Top | AnchorStyles.Right,
+                Location = new Point(btnAddFile.Left, btnAddFile.Bottom + ControlPadding),
+                Parent = this,
+                Text = "这是一个水印",
+                Width = 250
+            };
+            var btnAddWatermark = new Button
+            {
+                AutoSize = true,
+                Location = new Point(_txtWatermark.Right + ControlPadding, _txtWatermark.Top - 1),
+                Parent = this,
+                Text = "添加水印"
+            };
+            btnAddWatermark.Click += BtnAddWatermark_Click;
+
+            _txtLog = new TextBox
+            {
+                Anchor = AnchorStyles.Left | AnchorStyles.Bottom | AnchorStyles.Right,
+                Location = new Point(btnAddFile.Left, ClientSize.Height - ControlMargin - 200),
+                Multiline = true,
+                Parent = this,
+                ReadOnly = true,
+                ScrollBars = ScrollBars.Both,
+                Size = new Size(ClientSize.Width - 2 * btnAddFile.Left, 200),
+                WordWrap = false
+            };
+
+            _txtFileList = new TextBox
+            {
+                Anchor = AnchorStyles.Left | AnchorStyles.Top | AnchorStyles.Right | AnchorStyles.Bottom,
+                Location = new Point(btnAddFile.Left, _txtWatermark.Bottom + ControlPadding),
+                Multiline = true,
+                Parent = this,
+                ScrollBars = ScrollBars.Both,
+                Size = new Size(ClientSize.Width - 2 * btnAddFile.Left, _txtLog.Top - _txtWatermark.Bottom - 2 * ControlPadding),
+                WordWrap = false
+            };
+
+            var picAppreciate = new PictureBox
+            {
+                Cursor = Cursors.Hand,
+                Image = Resources.appreciatesmall,
+                Location = new Point(ClientSize.Width - 32 - 6, 6),
+                Parent = this,
+                Size = new Size(32, 32),
+                SizeMode = PictureBoxSizeMode.Zoom
+            };
+            picAppreciate.Click += PicAppreciate_Click;
+        }
+        #endregion
+    }
+}

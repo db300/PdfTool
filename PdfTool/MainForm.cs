844adad69e9dd4edbf4c1152a5f9634bdca9be37
DavidHawkes <db300@163.com>
2023-08-04 17:37:54 +0800

    新建重构项目

---
+++ PdfTool/MainForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PdfTool
+{
+    public partial class MainForm : Form
+    {
+        #region constructor
+        public MainForm()
+        {
+            InitializeComponent();
+
+            InitUi();
+        }
+        #endregion
+
+        #region ui
+        private void InitUi()
+        {
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = $"PDF工具 {System.Reflection.Assembly.GetExecutingAssembly().GetName().Version}";
+
+            var tabControl = new TabControl
+            {
+                Anchor = AnchorStyles.Left | AnchorStyles.Top | AnchorStyles.Right | AnchorStyles.Bottom,
+                Location = new Point(10, 10),
+                Parent = this,
+                Size = new Size(ClientSize.Width - 10 * 2, ClientSize.Height - 10 * 2)
+            };
+            tabControl.TabPages.AddRange(new TabPage[]
+            {
+                new TabPage("PDF拆分") { BorderStyle = BorderStyle.None },
+                new TabPage("PDF合并") { BorderStyle = BorderStyle.None }
+            });
+        }
+        #endregion
+    }
+}
---
+++ PdfTool/PdfSplitter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PdfTool
+{
+    public partial class PdfSplitter : UserControl
+    {
+        public PdfSplitter()
+        {
+            InitializeComponent();
+        }
+    }
+}

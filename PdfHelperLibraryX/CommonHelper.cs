8d10c27e1419a0fd44831a0dbf9cf54a7c429946
DavidHawkes <db300@163.com>
2023-08-28 15:37:08 +0800

    更新

---
+++ PdfHelperLibraryX/CommonHelper.cs
@@ -0,0 +1,16 @@
+﻿using PdfSharpCore.Pdf.IO;
+
+namespace PdfHelperLibraryX
+{
+    /// <summary>
+    /// 通用帮助类
+    /// </summary>
+    public static class CommonHelper
+    {
+        public static int GetPageCount(string inputPdfFileName)
+        {
+            var document = PdfReader.Open(inputPdfFileName, PdfDocumentOpenMode.ReadOnly);
+            return document.PageCount;
+        }
+    }
+}

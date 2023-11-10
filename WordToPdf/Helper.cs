feeb5de8f6c689131f1f503e4185c86833f79157
DavidHawkes <db300@163.com>
2023-11-10 18:02:14 +0800

    增加Word批量转PDF工具

---
+++ WordToPdf/Helper.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordToPdf
+{
+    internal static class Helper
+    {
+        // Convert method
+        internal static void ConvertToPdf(string input, string output, WdSaveFormat format = WdSaveFormat.wdFormatPDF)
+        {
+            // Create an instance of Word.exe
+            var oWord = new Application
+            {
+                // Make this instance of word invisible (Can still see it in the taskmgr).
+                Visible = false
+            };
+
+            // Interop requires objects.
+            object oMissing = System.Reflection.Missing.Value;
+            object isVisible = true;
+            object readOnly = true;     // Does not cause any word dialog to show up
+            //object readOnly = false;  // Causes a word object dialog to show at the end of the conversion
+            object oInput = input;
+            object oOutput = output;
+            object oFormat = format;
+
+            // Load a document into our instance of word.exe
+            _Document oDoc = oWord.Documents.Open(
+                ref oInput, ref oMissing, ref readOnly, ref oMissing, ref oMissing,
+                ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                ref oMissing, ref isVisible, ref oMissing, ref oMissing, ref oMissing, ref oMissing
+                );
+
+            // Make this document the active document.
+            oDoc.Activate();
+
+            // Save this document using Word
+            oDoc.SaveAs(ref oOutput, ref oFormat, ref oMissing, ref oMissing,
+                ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing
+                );
+
+            // Always close Word.exe.
+            oWord.Quit(ref oMissing, ref oMissing, ref oMissing);
+        }
+
+        internal static void ConvertToPdf(List<string> inputFileList, LogDelegate logHandler)
+        {
+            var background = new BackgroundWorker { WorkerReportsProgress = true };
+            background.DoWork += (ww, ee) =>
+            {
+                // Create an instance of Word.exe
+                var oWord = new Application { Visible = false };
+                // Interop requires objects.
+                object oMissing = System.Reflection.Missing.Value;
+                object isVisible = true;
+                object readOnly = true;     // Does not cause any word dialog to show up
+                object oFormat = WdSaveFormat.wdFormatPDF;
+                foreach (var fileName in inputFileList)
+                {
+                    object oInput = fileName;
+                    object oOutput = Path.Combine(Path.GetDirectoryName(fileName), $"{Path.GetFileNameWithoutExtension(fileName)}.pdf");
+                    // Load a document into our instance of word.exe
+                    var oDoc = oWord.Documents.Open(
+                        ref oInput, ref oMissing, ref readOnly, ref oMissing, ref oMissing,
+                        ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                        ref oMissing, ref isVisible, ref oMissing, ref oMissing, ref oMissing, ref oMissing);
+                    // Make this document the active document.
+                    oDoc.Activate();
+                    // Save this document using Word
+                    oDoc.SaveAs(ref oOutput, ref oFormat, ref oMissing, ref oMissing,
+                        ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                        ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing);
+                    background.ReportProgress(0, $"{fileName} 转换完成");
+                }
+                // Always close Word.exe.
+                oWord.Quit(ref oMissing, ref oMissing, ref oMissing);
+            };
+            background.ProgressChanged += (ww, ee) =>
+            {
+                if (ee.UserState is string msg) logHandler(msg);
+            };
+            background.RunWorkerCompleted += (ww, ee) =>
+            {
+                logHandler("转换完成");
+            };
+            background.RunWorkerAsync();
+        }
+
+        internal delegate void LogDelegate(string msg);
+    }
+}
---
+++ WordToPdf/MainForm.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WordToPdf.Properties;
+
+namespace WordToPdf
+{
+    public partial class MainForm : Form
+    {
+        #region constructor
+        public MainForm()
+        {
+            InitializeComponent();
+
+            InitUi();
+        }
+        #endregion
+
+        #region property
+        private readonly List<string> _inputWordFileList = new List<string>();
+        private TextBox _txtLog;
+
+        private const int ControlMargin = 20;
+        private const int ControlPadding = 12;
+        private const string Url4Appreciate = "https://www.yuque.com/docs/share/4d2ad434-a4fe-40a1-b530-c61811d5226e?# 《打赏说明》";
+        #endregion
+
+        #region event handler
+        private void BtnAddFile_Click(object sender, EventArgs e)
+        {
+            var openDlg = new OpenFileDialog { Filter = "Word文件(*.docx;*.doc)|*.docx;*.doc|所有文件(*.*)|*.*", Multiselect = true };
+            if (openDlg.ShowDialog() != DialogResult.OK) return;
+            _txtLog.Clear();
+            _inputWordFileList.Clear();
+            _inputWordFileList.AddRange(openDlg.FileNames.ToList());
+            foreach (var fileName in _inputWordFileList)
+            {
+                _txtLog.AppendText($"{fileName}\r\n");
+            }
+        }
+
+        private void BtnConvert_Click(object sender, EventArgs e)
+        {
+            if (_inputWordFileList.Count == 0)
+            {
+                _txtLog.AppendText("未添加需要转换的Word文件\r\n");
+                return;
+            }
+            _txtLog.AppendText($"正在转换，请稍候...\r\n");
+            Helper.ConvertToPdf(_inputWordFileList, msg =>
+            {
+                _txtLog.AppendText($"{msg}\r\n");
+            });
+        }
+
+        private void PicAppreciate_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(Url4Appreciate);
+        }
+        #endregion
+
+        #region ui
+        private void InitUi()
+        {
+            MaximizeBox = false;
+            ShowIcon = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = $"Word转PDF(批量) {System.Reflection.Assembly.GetExecutingAssembly().GetName().Version}";
+
+            var btnAddFile = new Button
+            {
+                AutoSize = true,
+                Location = new Point(ControlMargin, ControlMargin),
+                Parent = this,
+                Text = "添加文件"
+            };
+            btnAddFile.Click += BtnAddFile_Click;
+
+            var btnConvert = new Button
+            {
+                AutoSize = true,
+                Location = new Point(btnAddFile.Right + ControlPadding, btnAddFile.Top),
+                Parent = this,
+                Text = "开始转换"
+            };
+            btnConvert.Click += BtnConvert_Click;
+
+            _txtLog = new TextBox
+            {
+                Anchor = AnchorStyles.Left | AnchorStyles.Top | AnchorStyles.Right | AnchorStyles.Bottom,
+                Location = new Point(ControlMargin, btnAddFile.Bottom + ControlPadding),
+                Multiline = true,
+                Parent = this,
+                ReadOnly = true,
+                ScrollBars = ScrollBars.Both,
+                Size = new Size(ClientSize.Width - ControlMargin * 2, ClientSize.Height - ControlMargin - btnAddFile.Bottom - ControlPadding),
+                WordWrap = false
+            };
+
+            var picAppreciate = new PictureBox
+            {
+                Cursor = Cursors.Hand,
+                Image = Resources.appreciatesmall,
+                Location = new Point(ClientSize.Width - 32 - 6, 6),
+                Parent = this,
+                Size = new Size(32, 32),
+                SizeMode = PictureBoxSizeMode.Zoom
+            };
+            picAppreciate.Click += PicAppreciate_Click;
+        }
+        #endregion
+    }
+}

7410d2f0cf9626c47e374d6d40a71473ef555fe3
DavidHawkes <db300@163.com>
2024-02-21 15:28:25 +0800

    PDF帮助类库增加表格提取帮助类（基于Tabula+PdfPig）

---
+++ PdfHelperLibrary/TableExtractHelper.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Tabula.Extractors;
+using Tabula;
+using UglyToad.PdfPig;
+
+namespace PdfHelperLibrary
+{
+    /// <summary>
+    /// 表格提取帮助类
+    /// </summary>
+    public static class TableExtractHelper
+    {
+        public static (bool, string, List<PdfExtractTable>) ExtractTable(string inputPdfFileName)
+        {
+            var extractTables = new List<PdfExtractTable>();
+            using (var document = PdfDocument.Open(inputPdfFileName, new ParsingOptions() { ClipPaths = true }))
+            {
+                var oe = new ObjectExtractor(document);
+                var pageCount = document.NumberOfPages;
+                for (var i = 1; i <= pageCount; i++)
+                {
+                    var pageArea = oe.Extract(i);
+                    var ea = new SpreadsheetExtractionAlgorithm();
+                    var tables = ea.Extract(pageArea);
+                    var pdfExtractTables = tables.Select(t => new PdfExtractTable
+                    {
+                        Rows = t.Rows.Select(r => new PdfExtractRow
+                        {
+                            Cells = r.Select(c => c.GetText()).ToList()
+                        }).ToList()
+                    }).ToList();
+#if DEBUG
+                    System.Diagnostics.Debug.WriteLine(pdfExtractTables);
+#endif
+                    extractTables.AddRange(pdfExtractTables);
+                }
+            }
+            return (true, "", extractTables);
+        }
+    }
+
+    public class PdfExtractTable
+    {
+        public List<PdfExtractRow> Rows;
+    }
+
+    public class PdfExtractRow
+    {
+        public List<string> Cells;
+    }
+}

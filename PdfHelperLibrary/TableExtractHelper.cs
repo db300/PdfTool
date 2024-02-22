3ed20d7e01cfb50782f4b64d7f828241007475a3
DavidHawkes <db300@163.com>
2024-02-22 13:35:43 +0800

    Update TableExtractHelper.cs

--- PdfHelperLibrary/TableExtractHelper.cs
+++ PdfHelperLibrary/TableExtractHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Tabula;
 using Tabula.Extractors;
@@ -11,32 +12,87 @@ namespace PdfHelperLibrary
     /// </summary>
     public static class TableExtractHelper
     {
+        public static (bool, string, List<PdfExtractTable>) ExtractTable(Stream inputPdfStrean)
+        {
+            using (var document = PdfDocument.Open(inputPdfStrean, new ParsingOptions() { ClipPaths = true }))
+            {
+                var extractTables = ExtractTable(document);
+                return (true, "", extractTables);
+            }
+        }
+
         public static (bool, string, List<PdfExtractTable>) ExtractTable(string inputPdfFileName)
         {
-            var extractTables = new List<PdfExtractTable>();
             using (var document = PdfDocument.Open(inputPdfFileName, new ParsingOptions() { ClipPaths = true }))
             {
-                var oe = new ObjectExtractor(document);
-                var pageCount = document.NumberOfPages;
-                for (var i = 1; i <= pageCount; i++)
-                {
-                    var pageArea = oe.Extract(i);
-                    var ea = new SpreadsheetExtractionAlgorithm();
-                    var tables = ea.Extract(pageArea);
-                    var pdfExtractTables = tables.Select(t => new PdfExtractTable
-                    {
-                        Rows = t.Rows.Select(r => new PdfExtractRow
-                        {
-                            Cells = r.Select(c => c.GetText()).ToList()
-                        }).ToList()
-                    }).ToList();
-#if DEBUG
-                    System.Diagnostics.Debug.WriteLine(pdfExtractTables);
-#endif
-                    extractTables.AddRange(pdfExtractTables);
-                }
+                var extractTables = ExtractTable(document);
+                return (true, "", extractTables);
             }
-            return (true, "", extractTables);
+        }
+
+        public static (bool, string, List<PdfExtractRow>) ExtractTableRows(Stream inputPdfStrean)
+        {
+            using (var document = PdfDocument.Open(inputPdfStrean, new ParsingOptions() { ClipPaths = true }))
+            {
+                var extractTableRows = ExtractTableRows(document);
+                return (true, "", extractTableRows);
+            }
+        }
+
+        public static (bool, string, List<PdfExtractRow>) ExtractTableRows(string inputPdfFileName)
+        {
+            using (var document = PdfDocument.Open(inputPdfFileName, new ParsingOptions() { ClipPaths = true }))
+            {
+                var extractTableRows = ExtractTableRows(document);
+                return (true, "", extractTableRows);
+            }
+        }
+
+        private static List<PdfExtractTable> ExtractTable(PdfDocument document)
+        {
+            var extractTables = new List<PdfExtractTable>();
+            var oe = new ObjectExtractor(document);
+            var pageCount = document.NumberOfPages;
+            for (var i = 1; i <= pageCount; i++)
+            {
+                var pageArea = oe.Extract(i);
+                var ea = new SpreadsheetExtractionAlgorithm();
+                var tables = ea.Extract(pageArea);
+                var pdfExtractTables = tables.Select(t => new PdfExtractTable
+                {
+                    Rows = t.Rows.Select(r => new PdfExtractRow
+                    {
+                        Cells = r.Select(c => c.GetText()).ToList()
+                    }).ToList()
+                }).ToList();
+#if DEBUG
+                System.Diagnostics.Debug.WriteLine(pdfExtractTables);
+#endif
+                extractTables.AddRange(pdfExtractTables);
+            }
+            return extractTables;
+        }
+
+        private static List<PdfExtractRow> ExtractTableRows(PdfDocument document)
+        {
+            var rows = new List<PdfExtractRow>();
+            var oe = new ObjectExtractor(document);
+            var pageCount = document.NumberOfPages;
+            for (var i = 1; i <= pageCount; i++)
+            {
+                var pageArea = oe.Extract(i);
+                var ea = new SpreadsheetExtractionAlgorithm();
+                var tables = ea.Extract(pageArea);
+                tables.ForEach(table =>
+                {
+                    var rs = table.Rows.Select(r => new PdfExtractRow
+                    {
+                        Cells = r.Select(c => c.GetText()).ToList()
+                    }).ToList();
+                    rows.AddRange(rs);
+                });
+            }
+            return rows;
         }
     }
 

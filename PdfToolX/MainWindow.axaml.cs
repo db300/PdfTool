628c9776dbbb678eb033b079f785e10410086be8
DavidHawkes <db300@163.com>
2024-03-28 15:27:54 +0800

    PdfToolX增加超链接

--- PdfToolX/MainWindow.axaml.cs
+++ PdfToolX/MainWindow.axaml.cs
@@ -1,13 +1,45 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using System.Diagnostics;
 
 namespace PdfToolX;
 
 public partial class MainWindow : Window
 {
+    #region constructor
     public MainWindow()
     {
         InitializeComponent();
 
         Title = $"PDF工具集 {System.Reflection.Assembly.GetExecutingAssembly().GetName().Version}";
     }
+    #endregion
+
+    #region property
+    private const string Url4Appreciate = "https://www.yuque.com/docs/share/4d2ad434-a4fe-40a1-b530-c61811d5226e?# 《打赏说明》";
+    private const string Url4Feedback = "https://www.yuque.com/lengda/eq8cm6/ezwik4?singleDoc# 《需求记录》";
+    #endregion
+
+    #region event handler
+    private void TxtLink1_PointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        var psi = new ProcessStartInfo
+        {
+            FileName = Url4Appreciate,
+            UseShellExecute = true
+        };
+        Process.Start(psi);
+    }
+
+    private void TxtLink2_PointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        var psi = new ProcessStartInfo
+        {
+            FileName = Url4Feedback,
+            UseShellExecute = true
+        };
+        Process.Start(psi);
+    }
+    #endregion
 }

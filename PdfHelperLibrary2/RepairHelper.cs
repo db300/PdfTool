52bdec1d467dd11352b116b6d69271125a006ec2
DavidHawkes <db300@163.com>
2024-12-20 09:59:52 +0800

    增加PdfHelperLibrary2项目，基于iTextSharp

---
+++ PdfHelperLibrary2/RepairHelper.cs
@@ -0,0 +1,42 @@
+﻿using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdfHelperLibrary2
+{
+    /// <summary>
+    /// 修复帮助类
+    /// </summary>
+    public static class RepairHelper
+    {
+        public static string SaveAs(string inputFilename, out string outputFilename)
+        {
+            var dir = Path.GetDirectoryName(inputFilename);
+            var prefix = Path.GetFileNameWithoutExtension(inputFilename);
+            outputFilename = Path.Combine(dir, $"{prefix}-repaired-{DateTime.Now:yyyyMMddHHmmssfff}.pdf");
+            try
+            {
+                // 打开现有的 PDF 文档
+                using (var reader = new PdfReader(inputFilename))
+                {
+                    using (var fs = new FileStream(outputFilename, FileMode.Create, FileAccess.Write, FileShare.None))
+                    {
+                        using (var stamper = new PdfStamper(reader, fs))
+                        {
+                            // 这里可以对 pdfStamper 进行操作，例如添加水印、修改内容等
+                        }
+                    }
+                }
+                return "";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
